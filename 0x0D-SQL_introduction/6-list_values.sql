c58f4d4700bde042fd6a323c1fd250f26b0ec17d
adeolaakee <adeolaakee@gmail.com>
2022-08-10 05:09:28 -0700

    fixing last check

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,4 +1,4 @@
---lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
--- list all row in firs_table.
-
+-- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
+-- lists the rows a table in a database
 SELECT * FROM first_table;
+
