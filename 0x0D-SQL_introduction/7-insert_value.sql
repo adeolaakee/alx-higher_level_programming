7033b86aa78cb9cda6e9d137a0d28efc88a81e84
adeolaakee <adeolaakee@gmail.com>
2022-08-10 05:12:59 -0700

    fixing last check

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,5 @@
---inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
---New row:
+-- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+-- New row:
 -- id = 89
 -- name = Holberton School
 -- The database name will be passed as an argument of the mysql command
