dd1b6b70ae9adcae1f27e89680bb905d92610b50
adeolaakee <adeolaakee@gmail.com>
2022-08-10 08:48:18 -0700

    full creation of rows

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+
+CREATE TABLE IF NOT EXIST second_table
+INSERT INTO second_table
+(id = INT, name = VARCHAR(256),score = INT)
+
+VALUES(1,'John',score = 10)
+VALUES(2,'Alex',3)
+VALUES(3,'Bob',14)
+VALUES(4,'George',8);
