0607c19e46b08ae49df775617ed98eb1c8cf5e6f
adeolaakee <adeolaakee@gmail.com>
2022-08-10 03:07:51 -0700

    showing full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
---prints the full description of the table first_table from the hbtn_0c_0--
-
+--prints the full description of the table first_table from the hbtn_0c_0
 SHOW CREATE TABLE firts_table;
