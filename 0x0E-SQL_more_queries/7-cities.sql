cc724c1134654c8acda499969927e232a11ff976
adeolaakee <adeolaakee@gmail.com>
2022-08-11 10:21:31 -0700

    referencing

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,4 +6,4 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE AUTO_INCREMENT
        	NOT NULLL PRIMARY KEY,
 	state_id INT NOT NULL FOREIGN KEY(state_id)
-       	REFERENCES states(id))
+       	REFERENCES states(id));
