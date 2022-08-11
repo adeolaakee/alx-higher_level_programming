93f9a854d0890758bbf613462ec9609f4129c745
adeolaakee <adeolaakee@gmail.com>
2022-08-11 09:19:43 -0700

    database with user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,14 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2.
 
-CREATE DATABASE
+CREATE DATABASE 
 	IF NOT EXISTS hbtn_0d_2;
-
+-- creates a user
 CREATE USER
-	IF NOT EXISTS user_0d_2@localhost
+	IF NOT EXISTS user_0d_2@hbtn_0d_2 
 	IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-	ON hbtn_0d_2.*
-	TO user_0d_@localhost;
+-- grants SELECT privileges to a user
+GRANT SELECT 
+	ON hbtn_0d_2.* 
+	TO user_0d_2@hbtn_0d_2;
+
 
